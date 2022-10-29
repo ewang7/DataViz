e5fd72b350a5d68f57bcc9e411e5e1e3ce3e6a49
ewang8@luc <ewang8@luc.edu>
2022-10-29 08:39:47 -0500

    Added pieChart & Data Clean-up

--- eduScript.js
+++ eduScript.js
@@ -3,7 +3,7 @@
 
 
 /* Assign unique numeric ID to each td and canvas element */
-var blendedStats = document.querySelectorAll("td.blended");
+var blendedStats = document.querySelectorAll("td.blendedPercent");
 var blendedID = 0;
 
 for (var i = 0; i < blendedStats.length; i++) {
@@ -12,7 +12,7 @@ for (var i = 0; i < blendedStats.length; i++) {
 }
 
 
-var remoteStats = document.querySelectorAll("td.remote");
+var remoteStats = document.querySelectorAll("td.remotePercent");
 var remoteID = 1000;
 
 for (var i = 0; i < remoteStats.length; i++) {
@@ -20,7 +20,7 @@ for (var i = 0; i < remoteStats.length; i++) {
   remoteID++;
 }
 
-var canvasCollection = document.querySelectorAll("canvas");
+var canvasCollection = document.querySelectorAll("canvas.barGraphs");
 var canvasID = 2000;
 
 for (var i = 0; i < remoteStats.length; i++) {
@@ -28,21 +28,57 @@ for (var i = 0; i < remoteStats.length; i++) {
   canvasID++;
 }
 
-
-for (var i = 0; i < remoteStats.length; i++) {
-  console.log(document.getElementById(i+1000));
-}
-
-/* Data visualization via canvas */
+/* Data visualization via canvas for barGraphs */
 
 for (var i = 0; i < canvasCollection.length; i++) {
-  var context = document.getElementById(i+2000).getContext("2d");
+  var barGraphsContext = document.getElementById(i+2000).getContext("2d");
   var leftLength = document.getElementById(i).innerHTML;
   leftLength = leftLength.substring(0, leftLength.length - 1);
   var rightLength = document.getElementById(i+1000).innerHTML;
   rightLength = rightLength.substring(0, rightLength.length - 1);
-  context.fillStyle = "purple";
-  context.fillRect(0,0,parseInt(leftLength),20);
-  context.fillStyle = "green";
-  context.fillRect(parseInt(leftLength),0,parseInt(rightLength),20);
+  barGraphsContext.fillStyle = "purple";
+  barGraphsContext.fillRect(0,0,parseInt(leftLength),20);
+  barGraphsContext.fillStyle = "green";
+  barGraphsContext.fillRect(parseInt(leftLength),0,parseInt(rightLength),20);
 }
+
+
+
+/* Data visualization via canvas for pieChart */
+var blendedCountTotal = 0;
+var remoteCountTotal = 0;
+var enrollmentSumTotal = 0;
+
+var totalEnroll = document.querySelectorAll("td.totalEnroll");
+var blendedCount = document.querySelectorAll("td.blendedCount");
+var remoteCount = document.querySelectorAll("td.remoteCount");
+
+for (var i = 0; i < totalEnroll.length; i++) {
+  enrollmentSumTotal = enrollmentSumTotal + parseInt(totalEnroll[i].innerHTML);
+  blendedCountTotal = blendedCountTotal + parseInt(blendedCount[i].innerHTML);
+  remoteCountTotal = remoteCountTotal + parseInt(remoteCount[i].innerHTML);
+}
+
+console.log(enrollmentSumTotal);
+console.log(blendedCountTotal);
+console.log(remoteCountTotal);
+
+
+var pieChartContext = document.getElementById("pieChart").getContext("2d");
+pieChartContext.strokeStyle = "Purple";
+pieChartContext.fillStyle = "Purple";
+pieChartContext.lineWidth = 2;
+pieChartContext.beginPath();
+pieChartContext.arc(250,250,100,0,(blendedCountTotal / enrollmentSumTotal) * Math.PI * 2,true);
+pieChartContext.stroke();
+pieChartContext.fill();
+
+pieChartContext.strokeStyle = "Green";
+pieChartContext.fillStyle = "Green";
+pieChartContext.lineWidth = 2;
+pieChartContext.beginPath();
+pieChartContext.arc(250, 250,100,0,(remoteCountTotal / enrollmentSumTotal) * Math.PI * 2, false);
+pieChartContext.lineTo(250,250);
+pieChartContext.lineTo(350,250);
+pieChartContext.stroke();
+pieChartContext.fill();
--- remoteEdu.xsl
+++ remoteEdu.xsl
@@ -9,22 +9,31 @@
     <body>
       <h1>Learning Modality Preferences in NYC School Districts
       as of January 4th, 2021</h1>
+      <canvas id="pieChart" width="500px" height="500px">
+        Browser does not support HTML canvas
+      </canvas>
       <table>
         <tr>
           <th>School District Number</th>
           <th>Total Enrollment</th>
-          <th>Percent Blended/Hybrid Learning</th>
-          <th>Percent Remote Learning</th>
+          <th>Blended/Hybrid Learning Count</th>
+          <th>Remote Learning Count</th>
+          <th>Blended/Hybrid Learning Percent</th>
+          <th>Remote Learning Percent</th>
           <th>Data Visual</th>
         </tr>
         <xsl:for-each select="response/row">
+          <xsl:if test="total_enrollment &gt; 100">
           <tr>
             <td class="district"><xsl:value-of select="district"/></td>
             <td class="totalEnroll"><xsl:value-of select="total_enrollment"/></td>
-            <td class="blended"><xsl:value-of select="blended_1"/></td>
-            <td class="remote"><xsl:value-of select="remote_1"/></td>
-            <td><canvas width="100px" height="20px">Browser does not support HTML canvas</canvas></td>
+            <td class="blendedCount"><xsl:value-of select="blended"/></td>
+            <td class="remoteCount"><xsl:value-of select="remote"/></td>
+            <td class="blendedPercent"><xsl:value-of select="blended_1"/></td>
+            <td class="remotePercent"><xsl:value-of select="remote_1"/></td>
+            <td><canvas class="barGraphs" width="100px" height="20px">Browser does not support HTML canvas</canvas></td>
           </tr>
+          </xsl:if>
         </xsl:for-each>
       </table>
     </body>
